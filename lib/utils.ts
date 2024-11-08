53a37df9f6d85a600aab31af97b6334a4fb3a023
SamHalera <samuel.halera@gmail.com>
2024-11-08 18:01:18 +0100

    func to parsed text returning strong or italic

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,6 +16,26 @@ export const formatTitle = (text: string) => {
   });
 };
 
+export const parseText = (text: string) => {
+  const REGEX_STRONG = /\*\*(.*?)\*\*/g;
+  const REGEX_ITALIC = /\*(.*?)\*/g;
+
+  const italics = text.match(REGEX_ITALIC);
+  const strongs = text.match(REGEX_STRONG);
+  let parsedString: string = "";
+
+  if (italics && italics?.length > 0) {
+    parsedString = text.replace(REGEX_ITALIC, (_, p1) => {
+      return `<span class='italic'>${p1}</span>`;
+    });
+  }
+  if (strongs && strongs.length > 0) {
+    parsedString = text.replace(REGEX_STRONG, (_, p1) => {
+      return `<strong>${p1}</strong>`;
+    });
+  }
+  return parsedString;
+};
 export const extractErrorFieldFromErrorsObject = (
   error: any,
   keyToExtract: string
